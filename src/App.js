c2f5b8260c31943ba8e660cbf3d5802033361417
rish <rishabhr0712@gmail.com>
2023-05-14 13:28:45 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router";
 import { BrowserRouter, Link } from "react-router-dom";
 
 function App() {
+  alert("app initialised");
   const DefaultApp = () => {
     return (
       <div className="App">
